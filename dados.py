34703aafd8df680a70724a5c18bcd922e0dbb9c9
v1itor <49135561+v1itor@users.noreply.github.com>
2019-05-13 15:38:49 -0300

    Add files via upload
    Alterei e adicionei a descrição de algumas
     funções

--- dados.py
+++ dados.py
@@ -1,4 +1,4 @@
-#Meu primeiro programa
+#Este é o meu primeiro programa em Python!
 
 from random import randint #Importa a função de gerar números aleatórios
 
@@ -10,7 +10,7 @@ print("dado do player 1 :" , dado1)
 dado2 = randint(1,6)
 print("dado do player 2 :" , dado2)
 
-if dado1 > dado2:
+if dado1 > dado2: #}"if" Verifica se a variável dado1 é ">" maior que a variável dado2
     print("Player 1 ganhou")
 
 if dado2 > dado1:
