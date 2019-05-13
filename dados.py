049503198808ab2d9289a50e7000973f32ed05b4
v1itor <49135561+v1itor@users.noreply.github.com>
2019-05-13 16:17:40 -0300

    Add files via upload

--- dados.py
+++ dados.py
@@ -9,9 +9,16 @@ dados3 = randint(1, 6)
 print("Primeiro dado do Player 2:", dados3)
 dados4 = randint(1, 6)
 print("Segundo dado do player 2: ", dados4)
+
+jogador1 = dados1 + dados2
+jogador2 = dados3 + dados4
+
 print("Gerando resultado...")
 
-if dados1 + dados2 > dados3 + dados4:
+if jogador1 > jogador2:
     print("Player 1 win!")
-elif dados1 + dados2 < dados3 + dados4:
-    print("Player 2 win!")
+else:
+    if jogador1 < jogador2:
+        print("Player 2 win!")
+    else:
+        print("Empatou!")
