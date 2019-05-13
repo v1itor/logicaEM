a8268ea3f5563e53cdcf21d428c8aa21cfb84ea1
v1itor <49135561+v1itor@users.noreply.github.com>
2019-05-13 15:34:45 -0300

    Add files via upload
    Programa baseado em um jogo de dados, gera um número de 1~6 para 4 dados, soma dois dele e compara com a soma de outros dois

---
+++ dados.py
@@ -0,0 +1,17 @@
+#Meu primeiro programa
+
+from random import randint #Importa a função de gerar números aleatórios
+
+print("Boa sorte aos jogadores!")
+
+dado1 = randint(1,6) # gera um número entre 1 e 6 aleatoriamente
+print("dado do player 1 :" , dado1)
+
+dado2 = randint(1,6)
+print("dado do player 2 :" , dado2)
+
+if dado1 > dado2:
+    print("Player 1 ganhou")
+
+if dado2 > dado1:
+    print("player 2 ganhou")
